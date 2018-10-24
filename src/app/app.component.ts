98d12c730c576b49f076a3099af11aa99b6313ee
Rohan Arora <rohanraarora@gmail.com>
2018-10-24 16:17:02 +0530

    Add lifecycle methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'NinjasDemo';
+export class AppComponent implements OnInit, OnDestroy {
+
+  ngOnDestroy(): void {
+  }
+
+  ngOnInit(): void {
+
+  }
+
 }
