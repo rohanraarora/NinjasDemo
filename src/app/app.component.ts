58d1052246b850f3091c3ef52cab2ce57fda3962
Rohan Arora <rohanraarora@gmail.com>
2018-10-24 17:15:26 +0530

    Add basic Http request and loading status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Course} from '../models/course';
+import {catchError} from 'rxjs/operators';
+import {of} from 'rxjs';
+import {CoursesApiResponse} from '../models/courses-api.response';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +12,25 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit, OnDestroy {
 
+  courses: Course[];
+  loading: boolean;
+
+  constructor(private http: HttpClient) {
+  }
+
   ngOnDestroy(): void {
   }
 
   ngOnInit(): void {
-
+    this.loading = true;
+    this.http.get<CoursesApiResponse>('https://codingninjas.in/api/v3/courses').pipe(
+      catchError(error => {
+        console.log('Error', error);
+        return of({data: {courses: []}});
+      })).subscribe(response => {
+      this.loading = false;
+      this.courses = response.data.courses;
+    });
   }
 
 }
